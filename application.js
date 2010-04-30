e8716ffee9e3f923d45f68f27209a41c08d8d8cf
Russell Jones <spam@codeofficer.com>
2010-04-30 02:11:40 -0400

    puuuush

--- application.js
+++ application.js
@@ -2,7 +2,6 @@
 	$(function() {
 		
 		var $repositories = $('#repositories');
-		var $users = $('#users');
 		var $search_text = $('#search_text');
 		
 		// give the search box focus
@@ -11,18 +10,6 @@
 		$('#github_search').submit(function() {
 			// trick or treat
 			if ($search_text.val()=='') $search_text.val('codeofficer');
-			
-			// user search
-			// $.get('http://github.com/api/v1/json/' + escape($search_text.val()), function(json){				
-			// 	$.get('templates/user.ejs', function(template){
-			// 		$users.empty();
-			// 		$users.append("<li>" + "users" + "</li>");
-			// 		// $.each(json.repositories, function(index, repository) {
-			// 		// 	var html = $.srender(template, repository);
-			// 		// 	$repositories.append(html);
-			// 		// });
-			// 	});			
-			// });
 			
 			// repository search
 			$.get('http://github.com/api/v2/json/repos/search/' + escape($search_text.val()), function(json){				
