132db47e613b99f8ceba429ca5a0414c2a999887
Russell Jones <spam@codeofficer.com>
2010-04-30 01:54:37 -0400

    rawr

--- application.js
+++ application.js
@@ -12,9 +12,21 @@
 			// trick or treat
 			if ($search_text.val()=='') $search_text.val('codeofficer');
 			
+			// user search
+			// $.get('http://github.com/api/v1/json/' + escape($search_text.val()), function(json){				
+			// 	$.get('templates/user.ejs', function(template){
+			// 		$users.empty();
+			// 		$users.append("<li>" + "users" + "</li>");
+			// 		// $.each(json.repositories, function(index, repository) {
+			// 		// 	var html = $.srender(template, repository);
+			// 		// 	$repositories.append(html);
+			// 		// });
+			// 	});			
+			// });
+			
 			// repository search
-			$.get('http://github.com/api/v1/json/search/' + escape($search_text.val()), function(json){				
-				$.get('templates/project.ejs', function(template){
+			$.get('http://github.com/api/v2/json/repos/search/' + escape($search_text.val()), function(json){				
+				$.get('templates/repository.ejs', function(template){
 					$repositories.empty();
 					$.each(json.repositories, function(index, repository) {
 						var html = $.srender(template, repository);
